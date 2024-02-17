c5e5c5c0952e7a7f70d007cdc925294ae63c8426
RajeevKhati <rajeev.khati@nagarro.com>
2024-02-17 23:06:05 +0530

    feat(BE): add endpoint

--- src/shared/socket.js
+++ src/shared/socket.js
@@ -1,3 +1,8 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:5000");
+const BACKEND_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://sketch-book-backend-cit8.onrender.com"
+    : "http://localhost:5000";
+
+export const socket = io(BACKEND_URL);
